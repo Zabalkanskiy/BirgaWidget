3d1f69c5c6eaa3e72b2ab24ba54065f964d28814
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-07-14 13:49:17 +0300

    Fix error in string change "MARKETPRICE" in "LAST"

--- app/src/main/java/com/example/newfinamwidget/Retrofit/StockApi.kt
+++ app/src/main/java/com/example/newfinamwidget/Retrofit/StockApi.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface StockApi {
-    @GET("{stock}.json?iss.meta=off&iss.only=marketdata&marketdata.columns=MARKETPRICE")
+    @GET("{stock}.json?iss.meta=off&iss.only=marketdata&marketdata.columns=LAST")
     suspend fun getStock(@Path("stock") name : String) : MarketData
 }
